15e50f8d638c82db1638597182d1f246b476fe2c
Thibaud Giovannetti <tspoke@hotmail.fr>
2020-01-28 12:15:02 +0100

    Add missing export of payline payment management

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from "./models";
 import {Payline as InternalPayline} from "./services/payline";
 import {PaylineWeb as InternalPaylineWeb} from "./services/extensions/payline-web";
 import {PaylineWalletManagement as InternalPaylineWalletManagement} from "./services/extensions/payline-wallet-management";
+import {PaylinePaymentManagement as InternalPaylinePaymentManagement} from "./services/extensions/payline-payment-management";
 
 export const Payline = InternalPayline;
 export type Payline = InternalPayline;
@@ -14,3 +15,6 @@ export type PaylineWeb = InternalPaylineWeb;
 
 export const PaylineWalletManagement = InternalPaylineWalletManagement;
 export type PaylineWalletManagement = InternalPaylineWalletManagement;
+
+export const PaylinePaymentManagement = InternalPaylinePaymentManagement;
+export type PaylinePaymentManagement = InternalPaylinePaymentManagement;
